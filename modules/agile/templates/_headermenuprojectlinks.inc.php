2d1cf13e0aa298dd2456f0ff652d9b05d90e8a91
Sarah Kemp <thesarahkemp@gmail.com>
2026-04-24 08:00:01 -0700

    header link project to board not whiteboard

--- modules/agile/templates/_headermenuprojectlinks.inc.php
+++ modules/agile/templates/_headermenuprojectlinks.inc.php
@@ -6,7 +6,7 @@
             <li class="header"><?php echo __('Project boards'); ?></li>
             <?php if (count($boards)): ?>
                 <?php foreach ($boards as $board): ?>
-                    <li><a href="<?php echo make_url((!$tbg_user->hasProjectPageAccess('project_planning', $project) && $tbg_user->hasProjectPageAccess('project_only_planning', $project) ? 'agile_board' : 'agile_whiteboard'), array('project_key' => $board->getProject()->getKey(), 'board_id' => $board->getID())); ?>" class="<?php if ($tbg_request['board_id'] == $board->getID()) echo ' selected'; ?>"><?php echo $board->getName(); ?></a></li>
+                    <li><a href="<?php echo make_url((!$tbg_user->hasProjectPageAccess('project_planning', $project) && $tbg_user->hasProjectPageAccess('project_only_planning', $project) ? 'agile_board' : 'agile_board'), array('project_key' => $board->getProject()->getKey(), 'board_id' => $board->getID())); ?>" class="<?php if ($tbg_request['board_id'] == $board->getID()) echo ' selected'; ?>"><?php echo $board->getName(); ?></a></li>
                 <?php endforeach; ?>
             <?php else: ?>
                 <li class="disabled"><?php echo __('No project boards available'); ?></li>
